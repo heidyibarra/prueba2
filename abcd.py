7a6bd57f7ed7d724ebc1806eb0a005f17aab3d67
heidyibarra <heidyibarra88@gmail.com>
2017-03-08 14:59:33 -0500

    subiendo archivos en github

---
+++ abcd.py
@@ -0,0 +1,31 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('hola mundo')
+hola mundo
+>>> print(2+2)
+4
+>>> cadena="otorrinolaringologo"
+>>> for i in cadena:
+	print(i)
+
+	
+o
+t
+o
+r
+r
+i
+n
+o
+l
+a
+r
+i
+n
+g
+o
+l
+o
+g
+o
+>>> 
